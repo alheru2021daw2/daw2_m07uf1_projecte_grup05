71d887ca87f76a53a182dd04fee975fea8c5b8c4
alheru2021daw2 <72308281+alheru2021daw2@users.noreply.github.com>
2020-12-10 19:45:24 +0100

    Add files via upload

---
+++ classProducte.php
@@ -0,0 +1,20 @@
+<?php
+class producte{
+    public $seccio;
+    public $nom;
+    public $codi;
+    public $imatge;
+    public $preu;
+
+    public function __construct(){
+
+        $this->seccio = $s;
+        $this->nom = $n;
+        $this->codi = $c;
+        $this->imatge = $i;
+        $this->preu = $p;
+    }
+}
+$producte = new producte();
+
+?>
